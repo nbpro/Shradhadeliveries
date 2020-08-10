ba4f3b1746767e3587d1e161427638be5db78449
nbpro <nbneeraj8@gmail.com>
2020-08-10 21:52:40 +0530

    added features images

--- src/screens/features/features.js
+++ src/screens/features/features.js
@@ -4,18 +4,27 @@ import './features.css';
 const Features = () => {
   return (
     <section id="what_we_section" class="feature-section">
-      <div class="container w-screen">
+      <div class="w-full">
         <h2 class="text-water-blue">What we do</h2>
         <div class="description">
           <div class="flex">
             <div class="flex-1 text-center px-4 py-2 m-2">
-               This is first image
+              <img src="./assets/images/flower-grass-shradha.png"></img>
+              <p>
+                <h3 class="text-center">We pluck flowers</h3>
+              </p>
             </div>
             <div class="flex-1 text-center px-4 py-2 m-2">
-              This is second image
+              <img src="./assets/images/gift.png"></img>
+              <p>
+                <h3 class="text-center">Pack It up</h3>
+              </p>
             </div>
             <div class="flex-1 text-center px-4 py-2 m-2">
-              This is third image
+              <img src="./assets/images/home.png"  width="400" height="300"></img>
+              <p>
+                <h3 class="text-center">Deliver at your doorstep</h3>
+              </p>
             </div>
           </div>
         </div>
